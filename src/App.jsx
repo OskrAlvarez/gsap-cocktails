e507fc9fa31b63155fc43b67f8edfec08b040746
Oscardev <oscardev@gmail.com>
2025-10-18 17:26:14 -0400

    Implement the Menu Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <main>
+    <main >
       <Navbar />
       <Hero />
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 };
